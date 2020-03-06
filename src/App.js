32f83120e59ac63208d61a8b137ce2ede34a402e
Sofialav <cemeteryvixen@gmail.com>
2020-03-06 12:20:38 +0100

    post/message upd w/ local state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React, { Component } from "react";
 import superagent from "superagent";
 
 class App extends Component {
+  state = {
+    text: ""
+  };
   onSubmit = async e => {
     e.preventDefault();
     try {
       const response = await superagent
         .post("http://localhost:4000/message")
-        .send({ text: "hardcoded text" });
+        .send({ text: this.state.text });
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
-
+  onChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+  reset = () => {
+    this.setState({ text: "" });
+  };
   render() {
     return (
       <main>
         <form onSubmit={this.onSubmit}>
-          <input type="text"></input>
+          <input
+            type="text"
+            onChange={this.onChange}
+            value={this.state.text}
+          ></input>
           <button>Send</button>
         </form>
+        <button onClick={this.reset}>Reset</button>
       </main>
     );
   }
