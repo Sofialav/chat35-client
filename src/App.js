294fa5ba48b6aaa38435c02c22c1324bf79b3bfd
Sofialav <cemeteryvixen@gmail.com>
2020-03-06 12:01:20 +0100

    post/message created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import superagent from "superagent";
 
 class App extends Component {
+  onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await superagent
+        .post("http://localhost:4000/message")
+        .send({ text: "hardcoded text" });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
-    return <div>Hi!</div>;
+    return (
+      <main>
+        <form onSubmit={this.onSubmit}>
+          <input type="text"></input>
+          <button>Send</button>
+        </form>
+      </main>
+    );
   }
 }
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
-const enhancer =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+import ReduxThunk from "redux-thunk";
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : x => x;
+const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 const store = createStore(reducer, enhancer);
+
 export default store;
